6c71746981045f4f3824a0b9a5004f1fb4517a94
yamz-11 <wiratamajrz@gmail.com>
2022-12-05 16:35:33 +0700

    membuat project spring boot restful

---
+++ src/main/java/pws/c/resfulapi/ResfulapiApplication.java
@@ -0,0 +1,13 @@
+package pws.c.resfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResfulapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/c/resfulapi/ResfulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.resfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
